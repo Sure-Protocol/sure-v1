7c76a48059f8109efba9db4cb132c6077840e281
kristohberg <kristoffer.hovland.berg@gmail.com>
2022-06-10 15:51:18 +0200

    rollup dapp build

--- packages/app/rollup.config.js
+++ packages/app/rollup.config.js
@@ -4,6 +4,11 @@ import babel from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
+import typescript from '@rollup/plugin-typescript';
+import scss from 'rollup-plugin-scss';
+import svgr from '@svgr/rollup';
+import json from '@rollup/plugin-json';
+
 export default {
 	input: 'src/index.tsx',
 	output: {
@@ -12,8 +17,12 @@ export default {
 		sourcemap: true,
 	},
 	plugins: [
+		svgr(),
+		scss(),
+		typescript(),
+		json(),
 		nodeResolve({
-			extensions: ['.js', 'ts'],
+			extensions: ['.js', '.ts'],
 		}),
 		replace({
 			'process.env.NODE_ENV': JSON.stringify('development'),
