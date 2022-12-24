0f8cf061a2a8964a726554e839420ae39ee77852
kristohberg <kristoffer.hovland.berg@gmail.com>
2022-12-24 09:14:09 +0100

    fix: create mint, mint tokens and transfer to minter and proposer

--- programs/oracle/tests/test.rs
+++ programs/oracle/tests/test.rs
@@ -1,55 +1,43 @@
+use anchor_client::solana_sdk::signers::Signers;
 use anchor_client::{solana_sdk::signer::Signer, *};
 use anchor_lang::prelude::*;
 use anchor_lang::*;
 use oracle::id;
 use oracle::utils::SURE_ORACLE_CONFIG_SEED;
+use solana_program::hash::Hash;
 use solana_program::program_pack::Pack;
 use solana_program_test::*;
 use solana_sdk::*;
 use spl_token;
 
-#[tokio::test]
-async fn create_and_init() {
-    let mut program_test = ProgramTest::new("oracle", id(), processor!(oracle::entry));
-
-    // create program oracle owner
-    let oracle_owner = signature::Keypair::new();
+fn initialize_account(address: &Pubkey, program_test: &mut ProgramTest) {
     program_test.add_account(
-        oracle_owner.pubkey(),
+        *address,
         account::Account {
             lamports: 1_000_000_000,
             ..account::Account::default()
         },
     );
-    // create minter
-    let minter = signature::Keypair::new();
-    program_test.add_account(
-        minter.pubkey(),
-        account::Account {
-            lamports: 1_000_000_000,
-            ..account::Account::default()
-        },
-    );
-    let mut program_test_context = program_test.start_with_context().await;
+}
 
-    // create token mint - sure
-    let mint_account = signature::Keypair::new();
-
-    let token_program = spl_token::id();
-    let rent = program_test_context.banks_client.get_rent().await.unwrap();
+fn create_mint<T: Signer>(
+    minter: &T,
+    mint: &T,
+    rent: Rent,
+    recent_blockhash: Hash,
+) -> Result<solana_sdk::transaction::Transaction> {
     let mint_rent = rent.minimum_balance(spl_token::state::Mint::LEN);
-
-    // create mint instruction
+    let token_program = spl_token::id();
     let create_mint_account_ix = anchor_lang::solana_program::system_instruction::create_account(
-        &minter.pubkey(),       // from
-        &mint_account.pubkey(), // to account
+        &minter.pubkey(), // from
+        &mint.pubkey(),   // to account
         mint_rent,
         spl_token::state::Mint::LEN as u64,
         &token_program,
     );
     let create_mint_ix = spl_token::instruction::initialize_mint(
         &token_program.key(),
-        &mint_account.pubkey(),
+        &mint.pubkey(),
         &minter.pubkey(),
         Some(&minter.pubkey()),
         6,
@@ -57,33 +45,169 @@ async fn create_and_init() {
     .unwrap();
 
     // create mint transaction
-    let token_mint_tx = anchor_client::solana_sdk::transaction::Transaction::new_signed_with_payer(
-        &[create_mint_account_ix, create_mint_ix],
-        Some(&minter.pubkey()),
-        &[&minter, &mint_account],
-        program_test_context.last_blockhash,
-    );
+    Ok(
+        anchor_client::solana_sdk::transaction::Transaction::new_signed_with_payer(
+            &[create_mint_account_ix, create_mint_ix],
+            Some(&minter.pubkey()),
+            &[minter, mint],
+            recent_blockhash,
+        ),
+    )
+}
 
+fn mint_amount<T: Signer>(
+    minter: &T,
+    mint: &T,
+    ata: &Pubkey,
+    recent_blockhash: &Hash,
+    amount: u64,
+) -> Result<solana_sdk::transaction::Transaction> {
+    let mint_tokens_ix = spl_token::instruction::mint_to(
+        &spl_token::ID,
+        &mint.pubkey(),
+        &ata,
+        &minter.pubkey(),
+        &[&minter.pubkey(), &mint.pubkey()],
+        amount,
+    )?;
+
+    Ok(solana_sdk::transaction::Transaction::new_signed_with_payer(
+        &[mint_tokens_ix],
+        Some(&minter.pubkey()),
+        &[minter, mint],
+        *recent_blockhash,
+    ))
+}
+
+#[tokio::test]
+async fn create_veSure() {}
+
+#[tokio::test]
+async fn create_and_init() {
+    let mut program_test = ProgramTest::new("oracle", id(), processor!(oracle::entry));
+
+    // create program oracle owner
+    let oracle_owner = signature::Keypair::new();
+    initialize_account(&oracle_owner.pubkey(), &mut program_test);
+    // create minter
+    let minter = signature::Keypair::new();
+    initialize_account(&minter.pubkey(), &mut program_test);
+
+    let proposer = signature::Keypair::new();
+    initialize_account(&proposer.pubkey(), &mut program_test);
+
+    // start program
+    let mut program_test_context = program_test.start_with_context().await;
+    let rent = program_test_context.banks_client.get_rent().await.unwrap();
+
+    // create mint instruction
+    let mint = solana_sdk::signature::Keypair::new();
+    let mint_tx = create_mint(&minter, &mint, rent, program_test_context.last_blockhash).unwrap();
     // sign and send transaction
     program_test_context
         .banks_client
-        .process_transaction(token_mint_tx)
+        .process_transaction(mint_tx)
         .await
         .unwrap();
 
+    // create ata
+    let minter_ata = anchor_spl::associated_token::get_associated_token_address(
+        &minter.pubkey(),
+        &mint.pubkey(),
+    );
+    let create_ata_ix = spl_associated_token_account::create_associated_token_account(
+        &minter.pubkey(),
+        &minter.pubkey(),
+        &mint.pubkey(),
+    );
+    let create_ata_tx = solana_sdk::transaction::Transaction::new_signed_with_payer(
+        &[create_ata_ix],
+        Some(&minter.pubkey()),
+        &[&minter],
+        program_test_context.last_blockhash,
+    );
+
+    program_test_context
+        .banks_client
+        .process_transaction(create_ata_tx)
+        .await
+        .unwrap();
+
+    // mint some sure tokens
+    // create associated token address
+    let mint_tokens_tx = mint_amount(
+        &minter,
+        &mint,
+        &minter_ata,
+        &program_test_context.last_blockhash,
+        1_000_000_000_000,
+    )
+    .unwrap();
+
+    // TRANSACTION
+    program_test_context
+        .banks_client
+        .process_transaction(mint_tokens_tx)
+        .await
+        .unwrap();
+
+    // transfer token to proposer
+    let proposer_ata = anchor_spl::associated_token::get_associated_token_address(
+        &minter.pubkey(),
+        &mint.pubkey(),
+    );
+    let create_ata_ix = spl_associated_token_account::create_associated_token_account(
+        &proposer.pubkey(),
+        &proposer.pubkey(),
+        &mint.pubkey(),
+    );
+    let create_ata_tx = solana_sdk::transaction::Transaction::new_signed_with_payer(
+        &[create_ata_ix],
+        Some(&proposer.pubkey()),
+        &[&proposer],
+        program_test_context.last_blockhash,
+    );
+
+    program_test_context
+        .banks_client
+        .process_transaction(create_ata_tx)
+        .await
+        .unwrap();
+
+    let transfer_ix = spl_token::instruction::transfer(
+        &spl_token::ID,
+        &minter_ata,
+        &proposer_ata,
+        &minter.pubkey(),
+        &[&minter.pubkey()],
+        1_000_000_000,
+    )
+    .unwrap();
+
+    let transfer_tx = solana_sdk::transaction::Transaction::new_signed_with_payer(
+        &[transfer_ix],
+        Some(&minter.pubkey()),
+        &[&minter],
+        program_test_context.last_blockhash,
+    );
+    program_test_context
+        .banks_client
+        .process_transaction(transfer_tx)
+        .await
+        .unwrap();
+
+    // ======== ORACLE - INITIALIZE ORACLE =============
     let config_account = Pubkey::find_program_address(
         &[
             SURE_ORACLE_CONFIG_SEED.as_bytes().as_ref(),
-            mint_account.pubkey().as_ref(),
+            mint.pubkey().as_ref(),
         ],
         &oracle::id(),
     );
-
-    // intialize config
     let config = oracle::accounts::InitializeConfig {
         signer: oracle_owner.pubkey(),
         config: config_account.0.key(),
-        token_mint: mint_account.pubkey(),
+        token_mint: mint.pubkey(),
         system_program: anchor_lang::system_program::ID,
     };
 
